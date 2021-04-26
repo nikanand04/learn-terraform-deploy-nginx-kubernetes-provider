1e6fa2f2037c5d290f53e2ae0d5331f6acde57c0
Nikita Anand <51712876+nikanand04@users.noreply.github.com>
2021-04-26 17:16:40 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.42.0"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.1"
@@ -7,26 +11,30 @@ terraform {
   }
 }
 
-variable "host" {
-  type = string
+data "terraform_remote_state" "aks" {
+  backend = "remote"
+  config  = {
+    organization = "nikita_hashi"
+    workspaces   = {
+      name       = "terraform-provision-aks-cluster"
+    }
+  }
 }
 
-variable "client_certificate" {
-  type = string
+# Retrieve AKS cluster information
+provider "azurerm" {
+  features {}
 }
 
-variable "client_key" {
-  type = string
-}
-
-variable "cluster_ca_certificate" {
-  type = string
+data "azurerm_kubernetes_cluster" "cluster" {
+  name                = data.terraform_remote_state.aks.outputs.kubernetes_cluster_name
+  resource_group_name = data.terraform_remote_state.aks.outputs.resource_group_name
 }
 
 provider "kubernetes" {
-  host = var.host
+  host = data.azurerm_kubernetes_cluster.cluster.kube_config.0.host
 
-  client_certificate     = var.client_certificate
-  client_key             = var.client_key
-  cluster_ca_certificate = var.cluster_ca_certificate
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
 }
